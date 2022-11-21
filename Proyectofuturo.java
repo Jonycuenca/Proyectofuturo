50777a6597966a416efbc5e109cdf5147d7293c8
Jonattan <jcuencacerdan@gmail.com>
2022-11-21 23:58:37 +0100

    Inicio

---
+++ Proyectofuturo.java
@@ -0,0 +1,15 @@
+public class Proyectofuturo {
+    public static void main(String[] args) {
+        System.out.println("Estudiante" + args[0] + " : ");
+        System.out.println(veredicto(args[3],args[2]));
+    }
+    public static String veredicto(String valor1, String valor2){
+        if (valor1.equals(valor2)) {
+            return "Bien. Debes esforzarte un poco mas";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            return "Debes dedicar mas tiempo a estudiar";
+        }
+        return "Ideal. Continúa trabajando asi";
+    }
+}
